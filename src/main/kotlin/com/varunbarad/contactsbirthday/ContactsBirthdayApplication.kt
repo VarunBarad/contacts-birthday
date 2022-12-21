d44d7810b23e761ea0ce367f4c75956e8a31f48b
Varun Barad <varun@varunbarad.com>
2022-12-21 14:51:06 +0530

    Starter template from tutorial

---
+++ src/main/kotlin/com/varunbarad/contactsbirthday/ContactsBirthdayApplication.kt
@@ -0,0 +1,11 @@
+package com.varunbarad.contactsbirthday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContactsBirthdayApplication
+
+fun main(args: Array<String>) {
+    runApplication<ContactsBirthdayApplication>(*args)
+}
---
+++ src/test/kotlin/com/varunbarad/contactsbirthday/ContactsBirthdayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.varunbarad.contactsbirthday
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContactsBirthdayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
